ab3ce6e6140207ae0e54a275088bf72d0368279c
deep <isdsikder@gmail.com>
2026-05-25 08:27:42 +0530

    temp sign-out button

--- app/(roots)/(tabs)/profile.tsx
+++ app/(roots)/(tabs)/profile.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
+import { useAuth } from "@clerk/expo";
+import { useRouter } from "expo-router";
 
 export default function Profile() {
+  const router = useRouter();
+  const { signOut } = useAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      router.replace("/sign-in");
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+  ``;
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Profile</Text>
+      <TouchableOpacity
+        onPress={handleSignOut}
+        className="mt-4 px-4 py-2 bg-red-500 rounded-xl"
+      >
+        <Text>Sign Out</Text>
+      </TouchableOpacity>
     </View>
   );
 }
